8509887dc85f9324f7683ebc560207c529a767be
syed faris <132473241+syedfariskarachi@users.noreply.github.com>
2024-04-15 19:41:11 +0500

    currency converter program created by TypeScript

---
+++ index.ts
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency: any = {
+  USD: 1,
+  EUR: 0.93,
+  GBP: 0.76,
+  INR: 74.57,
+  PKR: 280,
+};
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency ",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter Amount",
+    type: "number",
+  },
+]);
+let from_amount = currency[user_answer.from];
+let to_amount = currency[user_answer.to];
+let amount = user_answer.amount;
+let final_amount = (amount / from_amount) * to_amount;
+
+console.log("Your total " + user_answer.to + " is = " + final_amount);
